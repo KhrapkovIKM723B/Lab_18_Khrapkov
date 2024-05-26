78c2aabbce467de1ab237de246e7ae2e4311c7b5
KhrapkovIKM723B <Ihor.Khrapkov@infiz.khpi.edu.ua>
2024-05-26 17:33:24 +0300

    Commit All

---
+++ Lab_18_Khrapkov.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+
+class Matrix
+{
+    int data[5];
+public:
+    Matrix()
+    {
+        for (int i = 0; i < 5; i++)
+            data[i] = 0;
+    }
+
+    Matrix(int arr[5])
+    {
+        for (int i = 0; i < 5; i++)
+            data[i] = arr[i];
+    }
+
+    void show()
+    {
+        for (int i = 0; i < 5; i++)
+            cout << data[i] << " ";
+        cout << endl;
+    }
+
+    Matrix operator+(const Matrix& m) 
+    {
+        Matrix temp;
+        for (int i = 0; i < 5; i++)
+            temp.data[i] = data[i] + m.data[i];
+        return temp;
+    }
+
+    Matrix operator-(const Matrix& m)
+    {
+        Matrix temp;
+        for (int i = 0; i < 5; i++)
+            temp.data[i] = data[i] - m.data[i];
+        return temp;
+    }
+
+    Matrix operator*(const Matrix& m)
+    {
+        Matrix temp;
+        for (int i = 0; i < 5; i++)
+            temp.data[i] = data[i] * m.data[i];
+        return temp;
+    }
+};
+
+int main() 
+{
+    setlocale(LC_ALL, "uk_UA.UTF-8");
+
+    int arr1[5] = { 1, 2, 3, 4, 5 };
+    int arr2[5] = { 5, 4, 3, 2, 1 };
+
+    Matrix m1(arr1), m2(arr2), m3;
+
+    cout << "Матриця 1: ";
+    m1.show();
+
+    cout << "Матриця 2: ";
+    m2.show();
+
+    m3 = m1 + m2;
+    cout << "Результат додавання: ";
+    m3.show();
+
+    m3 = m1 - m2;
+    cout << "Результат віднімання: ";
+    m3.show();
+
+    m3 = m1 * m2;
+    cout << "Результат множення: ";
+    m3.show();
+
+    return 0;
+}
+
